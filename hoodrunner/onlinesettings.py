5e5a463ddb8f66112ac12605cf8601330e5a5034
NicknineTheEagle <nik18.1995@gmail.com>
2025-02-14 16:58:11 +0300

    Added comments

--- hoodrunner/onlinesettings.py
+++ hoodrunner/onlinesettings.py
@@ -28,19 +28,23 @@ def application(environ, start_response):
         params+="[latestversion_changelist]%s" % ios_changelog
 
     # Added in 1.4.0
+    # Seasonal events (removed in 1.6.0)
     params+="[season]%s" % "normal" # Options: normal, halloween, xmas, easter
     params+="[end_season_datetime]%s" % "05-11-2012 00:00:00"
 
+    # Discounts
     #params+="[in_app_tier_1]%d" % 1000 # Positive value means +N bonus coins, negative value means -%N off
     #params+="[double_coin_discount]%d" % -50
     #params+="[discount_end_time]%d" % 1739602857 # UNIX timestamp
     #params+="[discount_deal_name]%s" % "Special offer"
 
+    # Ads
     #params+="[videoads_providerlist]%s" % "adcolony,vungleclips"
     #params+="[chartboost_delay_seconds]%d" % 30
     #params+="[videoads_defaultreward]%d" % 100
 
     # Added in 1.5.0
+    # Facebook integration and leaderboards
     params+="[social_report_min_seconds]%d" % 1200
     params+="[social_report_games_count]%d" % 5
     params+="[social_consolidate_min_seconds]%d" % 3600
